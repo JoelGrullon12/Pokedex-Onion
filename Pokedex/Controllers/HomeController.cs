edec276c91645a07911b91b27c159d16ac8937ef
JoelGrullon12 <jjj007507388@gmail.com>
2022-05-27 02:53:39 -0400

    Agregar archivos de proyecto.

---
+++ Pokedex/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pokedex.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
